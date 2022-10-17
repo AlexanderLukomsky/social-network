94c9b0f6103db09bafc8e2ae19a298a1d6d4fa7d
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-10-17 13:34:22 +0300

    feat:HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { store } from './redux/redux-store';
 
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
