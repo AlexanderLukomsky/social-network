a2964cf57c9fdd785b94b89fa3ad1a6f87019a9e
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-10-18 01:03:37 +0300

    feat: react lazy, react suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,20 @@
+import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Footer } from '../common/components/Footer/Footer';
 import { Music } from '../features/Music/Music';
 import { News } from '../features/News/News';
-import { Settings } from '../features/Settings/Settings';
 import { Sidebar } from '../features/Sidebar/Sidebar';
 import { useEffect } from 'react';
 import { LoginPage } from '../features/Login/Login';
 import { authThunk } from '../features/Login/auth-reducer';
 import { useAppDispatch } from '../redux/redux-store';
-import { Dialogs } from '../features/Dialogs/Dialogs';
 import { Profile } from '../features/Profile/Profile';
 import { Header } from '../common/components/Header/Header';
 import { Users } from '../features/users/Users';
 import './app.scss'
+const Settings = React.lazy(async () => ({ default: (await import('../features/Settings/Settings')).Settings }))
+const Dialogs = React.lazy(async () => ({ default: (await import('../features/Dialogs/Dialogs')).Dialogs }))
+
 export function App() {
   const dispatch = useAppDispatch()
   useEffect(() => {
@@ -27,11 +29,19 @@ export function App() {
           <Routes>
             <Route path='/profile/:userId' element={<Profile />} />
             <Route path='/profile' element={<Profile />} />
-            <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/dialogs' element={
+              <React.Suspense fallback={<div>loading...</div>}>
+                <Dialogs />
+              </React.Suspense>
+            } />
             <Route path='/users' element={<Users />} />
             <Route path='/news' element={<News />} />
             <Route path='/music' element={<Music />} />
-            <Route path='/settings' element={<Settings />} />
+            <Route path='/settings' element={
+              <React.Suspense fallback={<div>loading...</div>}>
+                <Settings />
+              </React.Suspense>
+            } />
             <Route path="/" element={<Navigate replace to="/profile" />} />
             <Route path="/login" element={<LoginPage />} />
           </Routes>
